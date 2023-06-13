b150f5fbbd3e70cb4d883f31cd9bfc1bb1d98e77
mobin <13594848919@163.com>
2023-06-13 21:49:08 +0800

    修改：实现动态高度的虚拟列表

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <VirtualScroll :list-data="data" :item-height="itemHeight" />
+  <!-- 模拟一个窄一点的列表 -->
+  <div style="width: 450px; height: 100%">
+    <VirtualScroll
+      :list-data="data"
+      :estimated-item-height="100"
+      #default="slotProps"
+    >
+      <VirtualItem :item="slotProps.item" />
+    </VirtualScroll>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { faker } from '@faker-js/faker'
 import VirtualScroll from '@/components/VirtualScroll.vue'
+import VirtualItem from '@/components/VirtualItem.vue'
 
 let data = ref([])
-let itemHeight = ref(100)
 
 // 生成随机数据
 function initData() {
   for (let i = 0; i < 1000; i++) {
     data.value.push({
       id: i,
-      value: 'test - ' + i
+      value: faker.lorem.sentences()
     })
   }
 }
